2bb51d4210879341ae63e2b084545c592b1cc8d5
Atuljadhav19 <133120719+Atuljadhav19@users.noreply.github.com>
2023-06-14 22:32:02 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here
 
+let bandNames = ['The Rolling Stones', 'Aerosmith', 'The Beatles', 'Red Hot Chili Peppers', 'AC/DC'];
+
+function sortBandNames(names) {
+  const articles = ['a', 'an', 'the'];
+
+  // Function to remove articles from a band name
+  function removeArticles(name) {
+    const words = name.split(' ');
+    const filteredWords = words.filter(word => !articles.includes(word.toLowerCase()));
+    return filteredWords.join(' ');
+  }
+
+  // Sorting the band names without articles
+  const sortedNames = names.sort((a, b) => {
+    const nameA = removeArticles(a);
+    const nameB = removeArticles(b);
+    return nameA.localeCompare(nameB);
+  });
+
+  // Generating the unordered list
+  const ulElement = document.getElementById('bands');
+  sortedNames.forEach(name => {
+    const liElement = document.createElement('li');
+    liElement.textContent = name;
+    ulElement.appendChild(liElement);
+  });
+}
+
+sortBandNames(bandNames);
